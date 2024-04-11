af90eb6231d1510604d08b3394fe1ae2b17d3edd
Pelegrint <iury.kassio@gmail.com>
2024-04-10 21:27:31 -0300

    second commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,12 +8,14 @@ const relogio = setInterval(function time(){
     let min = dateToday.getMinutes();
     let sec = dateToday.getSeconds();
 
-    if (hr < 10) hr = '0' + hr;
+    if (hr < 10){ hr = '0' + hr
+}
 
-    if (min < 10) min = '0' + min;
+    if (min < 10){ min = '0' + min
+}
 
-    if (sec < 10) sec = '0' + sec;
-
+    if (sec < 10) {sec = '0' + sec;
+}
     horas.textContent = hr;
     minutos.textContent = min;
     segundos.textContent = sec;
